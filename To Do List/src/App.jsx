ba06c03246d2d69a44c95bce96fd9c24b536a12d
Itskaif0 <mdkaif25121999@gmail.com>
2024-06-12 18:24:04 +0530

    first commit

---
+++ To Do List/src/App.jsx
@@ -0,0 +1,16 @@
+import { Form } from "./Components/form";
+
+function App() {
+  return (
+    <>
+      <div className="wrapper">
+        <h3>TODO APP</h3>
+        <div className="form-and-todo-box">
+          <Form />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
